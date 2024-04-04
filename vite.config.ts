dd4c8a1ca1ae0addd16f75da16f5f9d9a7ce1197
Luciana Urristi <lu_comu@hotmail.com>
2024-04-03 22:06:06 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Frontend-Globant-Egg/"
+  base: "https://lucianaurristi.github.io/Frontend-Globant-Egg/"
 })
